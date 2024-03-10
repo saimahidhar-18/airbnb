b412716627f2ef3f1e5285bb105666477909375e
M Sai Mahidhar <mahidharreddy2024@gmail.com>
2024-03-10 19:31:32 +0530

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css'
 function Footer() {
     return (
         <div className='footer'>
-            <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
-            <p>Privacy · Terms · Sitemap · Company Details</p>
+            <p> this is a demo @airbnb clone</p>
+            
         </div>
     )
 }
